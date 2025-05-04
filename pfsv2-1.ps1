106093350182f0ee6f76cc1a30d25823011d477f
Robbie Vos <vosrobbie4@gmail.com>
2025-05-04 20:35:55 +0200

    2e fout verbeterd

--- pfsv2-1.ps1
+++ pfsv2-1.ps1
@@ -5,6 +5,8 @@ Rename-NetAdapter -Name "Ethernet0" -NewName "LAN"
 New-NetIPAddress -IPAddress 192.168.101.12 -InterfaceAlias "LAN" -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway 192.168.101.11
 Set-DnsClientServerAddress -InterfaceAlias "LAN" -ServerAddresses ("192.168.101.11")
 
+install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
+Install-WindowsFeature -Name DNS -IncludeManagementTools
 Install-WindowsFeature DHCP -IncludeManagementTools
 Install-WindowsFeature Routing -IncludeManagementTools
 
